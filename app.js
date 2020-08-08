7637e81d24cb736a99194ef89f14eaa742dee58c
Ap <arunabhmaster@gmail.com>
2020-08-08 18:57:15 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/blog-backend', {
+        promiseLibrary: require('bluebird'),
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true
+    }).then(() => console.log('connection successfull'))
+    .catch((err) => console.log(err));
+
+var passport = require('passport');
+var auth = require('./routes/auth');
+var category = require('./routes/category');
+var post = require('./routes/post');
+
+
+
+
+var app = express();
+
+app.use(passport.initialize());
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/api/auth', auth);
+app.use('/api/category', category);
+app.use('/api/post', post);
+
+module.exports = app;
---
+++ models/Post.js
@@ -0,0 +1,19 @@
+const { schema } = require('./User');
+
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+var PostSchema = new mongoose.Schema({
+    category: { type: Schema.Types.ObjectId, ref: 'Category' },
+    id: String,
+    postTitle: String,
+    postAuthor: String,
+    postDesc: String,
+    postContent: String,
+    postReference: String,
+    postImgUrl: String,
+    created: { type: Date },
+    updated: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model('Post', PostSchema);
